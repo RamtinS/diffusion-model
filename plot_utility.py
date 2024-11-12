fcec7500dcdf0f0434e6b6fa73f7f4e1fbc045e8
Tobias Oftedal <oftedal03@gmail.com>
2024-11-12 23:43:04 +0100

    feat(plotting): use plot utility in the DDPM file

--- plot_utility.py
+++ plot_utility.py
@@ -1,19 +1,22 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
 
-def get_distribution(dataset):
+def get_distribution(dataset, *, show_plot = True):
     """Include the whole dataset in the parameter, such as MNIST, not just Y values"""
     _, mnist_distribution = np.unique([label for _, label in dataset],
                                       return_counts=True)
-    mnist_distribution = mnist_distribution
-    print(f"distribution sum is {mnist_distribution}")
-    plt.figure()
-    plt.title('MNIST Distribution')
-    plt.bar([i for i in range(10)],
-            [value.item() for value in mnist_distribution])
-    plt.show()
+    mnist_distribution = mnist_distribution / len(dataset)
+    if show_plot:
+        plt.figure()
+        plt.title('MNIST Distribution')
+        plt.bar([i for i in range(10)],
+                [value.item() for value in mnist_distribution])
+        plt.xticks(ticks=range(10))
+        plt.show()
 
     return mnist_distribution
 
@@ -27,9 +30,15 @@ def get_classification_distribution(*,
     image_size=28,
 ):
     image_count = input_batch.shape[0]
-
+    columns = 10
+    rows = math.ceil(image_count / columns)
     distributionSum = torch.zeros((1, 10), device=device)
-    fig, axs = plt.subplots(1, image_count, figsize=(image_count * 2, 2))
+
+    fig, axs = plt.subplots(rows, columns, figsize=(columns * 2, rows * 2)
+                           if show_plot
+                           else (None, None))
+
+    axs = axs.flatten()
 
     for i in range(image_count):
         single_image = torch.tensor(input_batch[i:i + 1]).to(device)
@@ -38,14 +47,20 @@ def get_classification_distribution(*,
             distributionSum += probability_distribution
 
         best_guess = probability_distribution.argmax(1).item()
-        axs[i].imshow(input_batch[i].reshape(image_size, image_size, channels),
-                      cmap="gray")
-        axs[i].axis("off")
-        axs[i].set_title(f"Best guess: {best_guess}")
+        if show_plot:
+            axs[i].imshow(input_batch[i].cpu().numpy().reshape(image_size, image_size, channels),
+                          cmap="gray")
+            axs[i].axis("off")
+            axs[i].set_title(f"Best guess: {best_guess}")
 
-    print(f"distribution sum is {distributionSum}")
-    plt.figure()
-    plt.title('Probability Distribution')
-    plt.bar([i for i in range(10)],
-            [value.item() for value in distributionSum[0]])
-    plt.show()
+    if show_plot:
+        for i in range(image_count, len(axs)):
+            axs[i].axis('off')
+    distributionSum = distributionSum / image_count
+
+    if show_plot:
+        plt.figure()
+        plt.title('Probability Distribution')
+        plt.bar([i for i in range(10)], [value.item() for value in distributionSum[0]])
+        plt.show()
+    return distributionSum
